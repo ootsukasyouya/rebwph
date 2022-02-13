d9f32b5dfc0c054b4614abddba3b29b14499b78c
ootsukasyouya <rikkuhose@gmail.com>
2022-02-13 23:40:34 +0900

    fail

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, :only => [:show]
   root to: 'home#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :stores, controllers: {
