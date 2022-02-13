9e0ae11d85a7a5e1a573e3e8dae063fda681c6df
ootsukasyouya <rikkuhose@gmail.com>
2022-02-14 00:33:25 +0900

    userマイページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/index'
   root to: 'home#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :stores, controllers: {
@@ -11,5 +13,6 @@ Rails.application.routes.draw do
     passwords:     'users/passwords',
     registrations: 'users/registrations'
   }
+  resources :users, :only => [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
